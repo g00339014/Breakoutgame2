df20dccb8614401622ffa13729f5353333c1daa8
g00339014 <36044039+g00339014@users.noreply.github.com>
2018-03-15 16:34:08 +0000

    draw bricks

--- js/breakout.js
+++ js/breakout.js
@@ -7,6 +7,15 @@ var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
 
+//draw bricks
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
@@ -15,6 +24,15 @@ var ballColour = "#0095DD"
 
 var rightPressed = false;
 var leftPressed = false;
+
+//hold bricks 2d
+var bricks = [];
+for(c=0; c<brickColumnCount; c++) {
+	bricks[c] = [];
+	for(r=0; r<brickRowCount; r++) {
+		bricks[c][r] = { x:0, y:0, status:1};
+	}
+}
 
 
 function drawBall() {
@@ -33,10 +51,28 @@ function drawPaddle() {
 	ctx.closePath();
 }
 
+//function for bricks
+function drawBricks() {
+	for(c=0; c<brickColumnCount; c++) {
+		for(r=0; r<brickRowCount; r++) {
+			var brickX = (c*(brickWidth+brickPadding)) + brickOffsetLeft;
+			var brickY = (r*(brickHeight+brickPadding)) + brickOffsetTop;
+			bricks[c][r].x = brickX;
+			bricks[c][r].y = brickY;
+			ctx.beginPath();
+			ctx.rect(brickX, brickY, brickWidth, brickHeight);
+			ctx.fillStyle = "#0095DD";
+			ctx.fill();
+			ctx.closePath;
+		}
+	}
+}
+
 function draw () {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
 	drawPaddle();
+	drawBricks();
 	x += dx;
 	y += dy;
 	
@@ -50,6 +86,7 @@ function draw () {
 	} else if(y + dy > canvas.height-ballRadius) {
 		
 		if(x > paddleX && x < paddleX + paddleWidth) {
+			
 			//paddle speed up
 			dy++;
 			dy = -dy;
