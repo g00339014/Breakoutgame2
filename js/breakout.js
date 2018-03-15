518f39cda5a0039e5eb290513ca7fb838f99e7a5
g00339014 <36044039+g00339014@users.noreply.github.com>
2018-03-15 13:47:27 +0000

    Drawing on the canvas

---
+++ js/breakout.js
@@ -0,0 +1,17 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+//set the starting point
+var x = canvas.width/2;
+var y = canvas.height-30;
+
+//draw the ball
+function draw() {
+	ctx.beginPath();
+	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
+
+setInterval(draw, 10);
